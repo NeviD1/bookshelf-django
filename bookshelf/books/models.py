0ba006bc2c5bdac20a627f64be12afebaac5faf0
NeviD1 <d.tsarapkin@mail.ru>
2022-06-07 21:54:54 +0300

    Добавлены Author, Book

--- bookshelf/books/models.py
+++ bookshelf/books/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+
+class Author(models.Model):
+    name = models.CharField(verbose_name='ФИО', max_length=200)
+
+    def __str__(self):
+        return self.name
+
+
+class Book(models.Model):
+    name = models.CharField(verbose_name='Название', max_length=300)
+    authors = models.ManyToManyField(Author)
+    description = models.TextField(verbose_name='Описание', max_length=10000, null=True, blank=True)
+
+    def __str__(self):
+        return self.name
